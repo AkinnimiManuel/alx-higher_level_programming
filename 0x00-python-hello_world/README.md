e0b3e7664c9ae02ec13b209fa464768027973894
AkinnimiManuel <akinnimiemmanuel@gmail.com>
2022-10-22 20:48:07 +0100

    changed files

--- 0x00-python-hello_world/README.md
+++ 0x00-python-hello_world/README.md
@@ -1,7 +1,1 @@
-### python programming language
-
-### Hello_world in python...
-
-### Task 1 is a Shell script that runs a Python script.
-
-### Task 2 is a Shell script that runs Python code.
+python class
--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,52 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-
-
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-    if operator == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
-    elif operator == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
-    elif operator == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-    elif operator == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
-    else:
-        print("Unknown operatorerator. Available operators: +, -, * and /")
-        exit(1)
-
-
 if __name__ == "__main__":
     from sys import argv
-    arg_calc(argv)
+    from calculator_1 import *
+
+    count = len(argv)
+
+    if count != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    op = argv[2]
+
+
+    def not_found():
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+
+    def add_():
+        total = add(num1, num2)
+        print("{:d} + {:d} = {:d}".format(num1, num2, total))
+        return total
+
+
+    def sub_():
+        total = sub(num1, num2)
+        print("{:d} - {:d} = {:d}".format(num1, num2, total))
+        return total
+
+
+    def mul_():
+        total = mul(num1, num2)
+        print("{:d} * {:d} = {:d}".format(num1, num2, total))
+        return total
+
+
+    def div_():
+        total = div(num1, num2)
+        print("{:d} / {:d} = {:d}".format(num1, num2, total))
+        return total
+
+
+    options = {
+        "+": add_,
+        "-": sub_,
+        "*": mul_,
+        "/": div_
+    }
+    options.get(op, not_found)()
