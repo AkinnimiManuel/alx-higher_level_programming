c5bd3ce2c6e94d2de93d0ddaeeefc98e873f41ff
Akinnimi Emmanuel <akinnimiemmanuel@gmail.com>
2022-10-24 20:45:42 +0100

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define function returns the list of attributes and methods of object"""
+
+
+def lookup(obj):
+    """Returns the methods and attributes of an object"""
+    return dir(obj)
+    
